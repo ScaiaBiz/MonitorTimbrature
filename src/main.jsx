c1b9a947f9cbee5aade787dff392edf562195cf6
Emilio Scaia <info@scaia.biz>
2025-04-05 11:59:14 +0200

    Inserito registrazione serviceworker

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+navigator.serviceWorker
+	.register('/MonitorTimbrature/service-worker.js')
+	.then(() => console.log('Service Worker registrato'))
+	.catch(err => console.error('Errore registrazione SW:', err));
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
